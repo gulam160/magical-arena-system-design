891220b33a734813e40b85f95a5307d4c047f9f7
gulam160 <gulam.mu159@gmail.com>
2023-11-18 23:40:20 +0530

    Added first test for player attrs

--- __test__/magicalArena.test.ts
+++ __test__/magicalArena.test.ts
@@ -1,7 +1,14 @@
 import { Player, MagicalArena } from "../src/index";
 
 // Test case for the Player class, attributes and its methods
-describe("Player", () => {});
+describe("Player", () => {
+  it("should correctly initialize player attributes", () => {
+    const player = new Player(50, 5, 10);
+    expect(player.health).toBe(50);
+    expect(player.strength).toBe(5);
+    expect(player.attack).toBe(10);
+  });
+});
 
 // Test case for the Magical Arena class
 describe("Magical Arena", () => {});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 /* Player class with the given attributes */
-class Player {}
+class Player {
+  health: number;
+  strength: number;
+  attack: number;
+
+  constructor(health: number, strength: number, attack: number) {
+    this.health = health;
+    this.strength = strength;
+    this.attack = attack;
+  }
+}
 
 /* Magical Arena class */
 class MagicalArena {}
