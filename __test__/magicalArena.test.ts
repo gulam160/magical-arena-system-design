b2707376654ddcfde2e82308d0eaa316a556351e
gulam160 <gulam.mu159@gmail.com>
2023-11-18 23:07:32 +0530

    Configured Jest a testing lib

---
+++ __test__/magicalArena.test.ts
@@ -0,0 +1,7 @@
+import { Player, MagicalArena } from "../src/index";
+
+// Test case for the Player class, attributes and its methods
+describe("Player", () => {});
+
+// Test case for the Magical Arena class
+describe("Magical Arena", () => {});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log(3 + 4);
+/* Player class with the given attributes */
+class Player {}
+
+/* Magical Arena class */
+class MagicalArena {}
+
+// Exporting classe to test in test file
+export { Player, MagicalArena };
