bd18e545e5194878b9f83b6b97e99b78431335e4
gulam160 <gulam.mu159@gmail.com>
2023-11-19 09:55:58 +0530

    Player:- Important methods and testcase done

--- __test__/magicalArena.test.ts
+++ __test__/magicalArena.test.ts
@@ -8,6 +8,31 @@ describe("Player", () => {
     expect(player.strength).toBe(5);
     expect(player.attack).toBe(10);
   });
+
+  it("should correctly roll the dice", () => {
+    const player = new Player(50, 5, 10);
+    jest.spyOn(Math, "random").mockReturnValue(0.5);
+    expect(player.rollTheDice()).toBe(4);
+  });
+
+  it("should correctly decrease health when taking damage", () => {
+    const player = new Player(50, 5, 10);
+    player.fightToDamage(20);
+    expect(player.health).toBe(30);
+  });
+
+  it("should not allow negative health", () => {
+    const player = new Player(10, 5, 10);
+    player.fightToDamage(20);
+    expect(player.health).toBe(0);
+  });
+
+  it("should correctly determine if the player is alive", () => {
+    const alivePlayer = new Player(50, 5, 10);
+    const deadPlayer = new Player(0, 5, 10);
+    expect(alivePlayer.isPlayerAlive()).toBe(true);
+    expect(deadPlayer.isPlayerAlive()).toBe(false);
+  });
 });
 
 // Test case for the Magical Arena class
--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,40 @@ class Player {
     this.strength = strength;
     this.attack = attack;
   }
+
+  /* Roll Dice Method */
+  rollTheDice(): number {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  fightToDamage(damage: number): void {
+    this.health -= damage;
+    if (this.health < 0) {
+      this.health = 0;
+    }
+  }
+
+  isPlayerAlive(): boolean {
+    return this.health > 0;
+  }
 }
 
 /* Magical Arena class */
-class MagicalArena {}
+class MagicalArena {
+  players: Player[];
+  currentPlayerIndex: number;
+
+  constructor(player1: Player, player2: Player) {
+    this.players = [player1, player2];
+    this.currentPlayerIndex = 0;
+  }
+
+  switchTurn(): void {
+    this.currentPlayerIndex = 1 - this.currentPlayerIndex;
+  }
+}
+const playerA = new Player(50, 5, 10);
+const playerB = new Player(100, 10, 5);
 
 // Exporting classe to test in test file
 export { Player, MagicalArena };
